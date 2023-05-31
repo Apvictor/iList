40addfd3f77519be40aec53c7100866fe2aba3e7
Apvictor <armando2019ti@gmail.com>
2023-05-31 19:03:37 -0300

    fix: alterar pacote do aplicativo

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/realmdbapp/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/ilist/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
